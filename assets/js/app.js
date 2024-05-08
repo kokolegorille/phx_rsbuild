37d1ad6c010b5e3de05d25668fe98d18a2e0411a
kokolegorille <koko.le.gorille@gmail.com>
2024-05-09 01:12:30 +0200

    Update to liveview 1.0 rc0

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,13 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "topbar"
+import phxFeedbackDom from "./phx_feedback_dom"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+    params: {_csrf_token: csrfToken},
+    dom: phxFeedbackDom({})
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
